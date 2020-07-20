d7c0f67d71d7239fa31ae3c7a841006062ece06c
Christopher Siu <christopher@siu.sh>
2020-07-20 13:20:13 -0700

    Example of nexted subroutine calls.

--- 07.20_subroutines/nested.asm
+++ 07.20_subroutines/nested.asm
@@ -1,9 +1,13 @@
         .ORIG x3000
-MAIN    JSR FOO
-        HALT
+MAIN    JSR FOO     ; 1. MAIN calls FOO.
+        HALT        
 
-FOO     JSR BAR
-        RET
+FOO     ST R7, SAVER7
+        JSR BAR     ; 2. FOO calls BAR.
+        LD R7, SAVER7
+        RET         ; 4. FOO returns to MAIN.
 
-BAR     RET
+SAVER7  .FILL x0000
+
+BAR     RET         ; 3. BAR returns to FOO.
         .END

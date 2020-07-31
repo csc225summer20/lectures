f3f118cd5ca50b933dadc0ecd92292ab9a496508
Christopher Siu <christopher@siu.sh>
2020-07-31 13:13:16 -0700

    Example of C functions and prototypes.

---
+++ 07.31_functions/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* To include the information in "add.h": */
+#include "add.h"
+
+int main(void) {
+    /* To call a function that takes two ints and returns an int: */
+    printf("%d\n", add(1, 2));
+
+    return 0;
+}

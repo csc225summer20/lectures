ebd73b503fb42d3b4d60f5190cb0714cb5cb3526
Christopher Siu <christopher@siu.sh>
2020-08-05 13:01:43 -0700

    Example of recursive assembly.

--- 08.03_runtimestack/add.asm
+++ 08.03_runtimestack/add.asm
@@ -4,7 +4,22 @@
 MAINFN  LD  R6, INITSP  ; Init. the stack pointer.
         ADD R5, R6, #0  ; Init. the frame pointer.
 
-                        ; Call "add".
+        ; Caller setup:
+        AND R0, R0, #0  ; Push "2".
+        ADD R0, R0, #2
+        ADD R6, R6, #-1
+        STR R0, R6, #0
+        ADD R0, R0, #-1 ; Push "1".
+        ADD R6, R6, #-1
+        STR R0, R6, #0
+
+        JSR ADDFN       ; Call "add".
+
+        ; Caller teardown:
+        LDR R1, R6, #0  ; Pop the return value.
+        ADD R6, R6, #1
+        ADD R6, R6, #1  ; Pop "1".
+        ADD R6, R6, #1  ; Pop "2".
 
         LEA R0, STRING  ; NOTE: "printf" is really another function call, but
         PUTS            ;       it's defined in stdio, and its implementation
@@ -23,5 +38,29 @@ NEWLINE .FILL x0A
 
 
 ; int add(int, int)
+ADDFN   ; Callee setup:
+        ADD R6, R6, #-1 ; Push space for the return value.
+        ADD R6, R6, #-1 ; Push the return address.
+        STR R7, R6, #0
+        ADD R6, R6, #-1 ; Push the dynamic link.
+        STR R5, R6, #0
+        ADD R5, R6, #-1 ; Set the frame pointer.
+        ADD R6, R6, #-1 ; Push "z".
+
+        ; Callee body:
+        LDR R0, R5, #4  ; Load "x" into R0.
+        LDR R1, R5, #5  ; Load "y" into R1.
+        ADD R2, R0, R1  ; Compute "x + y".
+        STR R2, R5, #0  ; Set "z = x + y".
+
+        ; Callee teardown:
+        LDR R0, R5, #0  ; Load "z" into R0.
+        STR R0, R5, #3  ; Set the return value to "z".
+        ADD R6, R6, #1  ; Pop "z".
+        LDR R5, R6, #0  ; Pop the dynamic link.
+        ADD R6, R6, #1
+        LDR R7, R6, #0  ; Pop the return address.
+        ADD R6, R6, #1
+        RET             ; Return.
 
         .END

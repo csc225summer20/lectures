530ce9868a6ccbc4c2b2bca356970508f68184c0
Christopher Siu <christopher@siu.sh>
2020-08-10 13:09:08 -0700

    Examples of arrays in functions.

--- 08.10_arrays/passArray.c
+++ 08.10_arrays/passArray.c
@@ -1,4 +1,8 @@
 void foo(int a2[]) {
+
+    /* NOTE: The value of an array is the address of its first element, so this
+     *       modifies "a1" within "main": */
+
     a2[0] = 5;
 }
 
--- 08.10_arrays/returnArray.c
+++ 08.10_arrays/returnArray.c
@@ -1,5 +1,9 @@
 int* foo() {
     int a2[2];
+
+    /* NOTE: The value of an array is the address of its first value, so this
+     *       returns a pointer to the deallocated local variable "a2[0]": */
+
     return a2;
 }
 
